f9870ee3e298d2e36c2f5ebdc104073124305d70
Skryam <skryams@gmail.com>
2024-10-18 03:21:12 +0500

    update

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import fastify from 'fastify';
 const app = fastify();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/users', (req, res) => {
+  res.send('GET /users');
+});
+
+app.post('/users', (req, res) => {
+  res.send('POST /users');
 });
 
 app.listen({ port }, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+app.get('/hello', (req, res) => {
+  const name = req.query.name;
+  if (!name) {
+    res.send('Hello, World!')
+  } else {
+    res.send(`Hello, ${name}!`)
+  }
+})
