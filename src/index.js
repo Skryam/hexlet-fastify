f328f7be5a127d72e4700f3e2cf9fafc78d3a106
Skryam <skryams@gmail.com>
2024-10-29 17:29:50 +0500

    u

--- src/index.js
+++ src/index.js
@@ -5,22 +5,35 @@ import sanitize from 'sanitize-html';
 import formbody from '@fastify/formbody';
 import { plugin as fastifyReverseRoutes } from 'fastify-reverse-routes';
 import addRoutes from './routes/index.js';
+import fastifyCookie from '@fastify/cookie';
 
 export default async () => {
   const app = fastify({ exposeHeadRoutes: false });
 
   await app.register(fastifyReverseRoutes);
+
   const route = (name, placeHoldersValues) => app.reverse(name, placeHoldersValues);
+
   await app.register(view, {
     engine: { pug },
     defaultContext: {
       route,
     },
+    root: 'src/views'
   });
+
   await app.register(formbody);
 
+  await app.register(fastifyCookie);
+
   app.get('/', { name: 'index' }, (req, res) => {
-    res.view('src/views/index')
+    const visited = req.cookies.visited;
+    const data = {
+      visited,
+    };
+    res.cookie('visited', true);
+
+    res.view('index', data);
   });
 
   addRoutes(app);
