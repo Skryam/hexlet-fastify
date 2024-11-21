b690c34b35a0df39f4876470ecb89074dc074854
Skryam <skryams@gmail.com>
2024-11-21 11:34:59 +0500

    u

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,6 @@ import build from '../src/index.js';
 const app = await build();
 const port = process.env.PORT || 3000;
 
-app.listen({ port }, () => {
+app.listen({ host: '0.0.0.0', port }, () => {
   console.log(`Example app listening on port ${port}`);
 });
